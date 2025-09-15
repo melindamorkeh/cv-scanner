aac10631b8027ea5a4734214206fb48211441507
zain-ahmed <zain.auk05@gmail.com>
2025-09-15 01:26:48 +0100

    Minor - Add urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,5 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("login/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", MeView.as_view(), name="me"),
 ]
